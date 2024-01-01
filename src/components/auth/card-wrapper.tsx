02dac1423007d193b805d5288e71239f15b64830
Jean Arruda <jeanarruda@MacBook-Pro-de-Jean.local>
2024-01-01 15:47:31 -0300

    feat: create auth page

---
+++ src/components/auth/card-wrapper.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { BackButton } from "@/components/auth/back-button";
+import { Header } from "@/components/auth/header";
+import { Social } from "@/components/auth/social";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+const CardWrapper = ({
+  backButtonHref,
+  backButtonLabel,
+  children,
+  headerLabel,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CardWrapper;
